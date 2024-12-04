b1fcccde9e796e3d4674ce09ecf0e88049192541
stockslager <149531737+stockslager@users.noreply.github.com>
2024-12-04 10:58:49 -0500

    Create library_params_v3.js

---
+++ library_params_v3.js
@@ -0,0 +1,7 @@
+// root directory for inat observations
+let root_observations = 'https://www.inaturalist.org/observations/';
+let root_people       = 'https://www.inaturalist.org/people/';
+let root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
+
+// repo root directory
+let github_root = 'https://stockslager.github.io/iNat/';
