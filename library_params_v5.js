73d0e0d3ca2771ad01b54e35d50b9d5faca6b550
stockslager <149531737+stockslager@users.noreply.github.com>
2025-02-18 16:47:02 -0500

    Create library_params_v5.js

---
+++ library_params_v5.js
@@ -0,0 +1,19 @@
+// root directory for inat observations
+let root_observations = 'https://www.inaturalist.org/observations/';
+let root_people       = 'https://www.inaturalist.org/people/';
+let root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
+let root_projects     = 'https://www.inaturalist.org/projects/';
+
+// repo root directory
+let github_root = 'https://stockslager.github.io/iNat/';
+
+// iconic taxa id defaults
+let iPlants = '47126';
+let iReptiles = '26036';
+let iAmphibians = '20978';
+let iBirds  = '3';
+let iMOlluscs = '47115';
+let iFungi = '47170';
+let iMammals = '40151';
+let iSpiders = '47118';
+let iInsects = '47158';
