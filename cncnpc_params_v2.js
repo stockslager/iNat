ce561e22c14a472f748a81df93f69bef21bf2258
stockslager <149531737+stockslager@users.noreply.github.com>
2025-02-19 14:14:12 -0500

    Create cncnpc_params_v2.js

---
+++ cncnpc_params_v2.js
@@ -0,0 +1,23 @@
+// root directory for inat observations
+let root_observations = 'https://www.inaturalist.org/observations/';
+let root_people       = 'https://www.inaturalist.org/people/';
+let root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
+let root_projects     = 'https://www.inaturalist.org/projects/';
+
+// repo root directory
+let github_root = 'https://stockslager.github.io/iNat/';
+
+// backyard affiliate defaults
+let cnc_params       = '&view=visitingonly&g_list=4488957&s_list=4488958';
+let grovedale_params = '&view=visitingonly&g_list=4484313&s_list=4484172';
+  
+// iconic taxa id defaults
+let iPlants = '47126';
+let iReptiles = '26036';
+let iAmphibians = '20978';
+let iBirds  = '3';
+let iMOlluscs = '47115';
+let iFungi = '47170';
+let iMammals = '40151';
+let iSpiders = '47118';
+let iInsects = '47158';
