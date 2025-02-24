b4b92db07a934ee32328dc1ed078970586c4c4ff
stockslager <149531737+stockslager@users.noreply.github.com>
2025-02-23 22:06:33 -0500

    Create library_params_v6.js

---
+++ library_params_v6.js
@@ -0,0 +1,22 @@
+// root directory for inat observations
+let root_observations = 'https://www.inaturalist.org/observations/';
+let root_people       = 'https://www.inaturalist.org/people/';
+let root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
+let root_projects     = 'https://www.inaturalist.org/projects/';
+
+// repo root directory
+let github_root = 'https://stockslager.github.io/iNat/';
+
+// usda outlink used in cncnpc_seeds_fields.html
+let root_usda = 'https://plants.usda.gov/plant-profile/';
+
+// iconic taxa id defaults
+let iPlants = '47126';
+let iReptiles = '26036';
+let iAmphibians = '20978';
+let iBirds  = '3';
+let iMOlluscs = '47115';
+let iFungi = '47170';
+let iMammals = '40151';
+let iSpiders = '47118';
+let iInsects = '47158';
