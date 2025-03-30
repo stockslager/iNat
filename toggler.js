ee32731e1db355f5225f2b680d6a698909bfa4c2
stockslager <149531737+stockslager@users.noreply.github.com>
2025-03-30 16:12:09 -0400

    Update toggler.js

--- toggler.js
+++ toggler.js
@@ -2,11 +2,11 @@
   "fields": [
     {
       "field_id": "2262",
-      "field_name": "smell",
+      "field_name": "smell"
     },
     {
       "field_id": "2487",
-      "field_name": "odor",
+      "field_name": "odor"
     }
   ]
 }
