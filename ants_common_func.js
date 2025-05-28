a5e8c6d98954b73022652c3951326fd495131355
stockslager <149531737+stockslager@users.noreply.github.com>
2025-05-28 07:03:25 -0400

    Update ants_common_func.js

--- ants_common_func.js
+++ ants_common_func.js
@@ -1,6 +1,5 @@
 function getHomeOpts( winurlparams ) {
    let opts='';
-   // build param list for url's used in fresults
    for( const [key, value] of winurlparams.entries() ) { 
         if( key !== 'taxon_id' && !key.startWith('field:') ) {
             if( opts === '' ){
