7e2601d5639cfac3fb7eb3698252e1119b36218a
stockslager <149531737+stockslager@users.noreply.github.com>
2025-05-28 07:02:51 -0400

    Update ants_common_func.js

--- ants_common_func.js
+++ ants_common_func.js
@@ -1,22 +1,22 @@
 function getHomeOpts( winurlparams ) {
-  let home_opts = '';
+   let opts='';
+   // build param list for url's used in fresults
+   for( const [key, value] of winurlparams.entries() ) { 
+        if( key !== 'taxon_id' && !key.startWith('field:') ) {
+            if( opts === '' ){
+                opts += "?";
+            } else {
+                opts += "&";
+            }
+            opts += key;
+            opts += "=";
 
-  let p_project_id = winurlparams.get('project_id') || '';
-  let p_taxon_id   = winurlparams.get('taxon_id') || ''; 
-  let p_params     = winurlparams.get('params') || '';
-  let p_place_id   = winurlparams.get('place_id') || '';
-
-  if( p_project_id ) {
-      home_opts = '?project_id='+p_project_id;
-      if( p_taxon_id ) {
-          home_opts += '&taxon_id='+p_taxon_id;
-      }
-  } else {
-      home_opts = '?taxon_id='+p_taxon_id;
-  }
-  
-  if( p_params )      { home_opts += '&params='+p_params; }
-  if( p_place_id )    { home_opts += '&place_id='+p_place_id; }
-
-  return home_opts;
+            if( key === 'page' ){
+                opts += '1';
+            } else {
+                opts += value;
+            }
+        }
+   } 
+   return( opts );
 }
