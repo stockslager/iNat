a8eb70a59227b88deff45fe09790f076ff922005
stockslager <149531737+stockslager@users.noreply.github.com>
2025-05-28 06:43:16 -0400

    Update ants_common_func.js

--- ants_common_func.js
+++ ants_common_func.js
@@ -1,6 +1,11 @@
 function getHomeOpts( winurlparams ) {
   let home_opts = '';
 
+  let p_project_id = winurlparams.get('project_id') || '';
+  let p_taxon_id   = winurlparams.get('taxon_id') || ''; 
+  let p_params     = winurlparams.get('params') || '';
+  let p_place_id   = winurlparams.get('place_id') || '';
+
   if( p_project_id ) {
       home_opts = '?project_id='+p_project_id;
       if( p_taxon_id ) {
@@ -9,9 +14,9 @@ function getHomeOpts( winurlparams ) {
   } else {
       home_opts = '?taxon_id='+p_taxon_id;
   }
-  if( p_params ) {
-      home_opts += '&params='+p_params;
-  }
+  
+  if( p_params )      { home_opts += '&params='+p_params; }
+  if( p_place_id )    { home_opts += '&place_id='+p_place_id; }
 
   return home_opts;
 }
