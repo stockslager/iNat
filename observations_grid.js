aa8e8d085e42552a80a1db48d1551ee0d738e703
stockslager <149531737+stockslager@users.noreply.github.com>
2025-05-29 22:48:55 -0400

    Update observations_grid.js

--- observations_grid.js
+++ observations_grid.js
@@ -28,12 +28,6 @@ function buildGrid(data) {
          }
          else {
             let obs = results[obsseq];
-            let taxon2 = '(Unknown Taxon)';
-            for( let j=0; j<obs.ofvs.length; j++ ) {
-                 if( p_action === 'on' && obs.ofvs[j].field_id === 7623 ) {   // plant that the organism was found on
-                     taxon2 = obs.ofvs[j].taxon?(obs.ofvs[j].taxon.preferred_common_name?(obs.ofvs[j].taxon.preferred_common_name):obs.ofvs[j].taxon.name):'(Unknown Taxon)';
-                 }
-            }
             let photourl = (obs.photos.length>0) ? obs.photos[0].url : null;
             let taxon = obs.taxon?(obs.taxon.preferred_common_name?(obs.taxon.preferred_common_name):obs.taxon.name):'(Unknown Taxon)';
             let user = obs.user.login+((obs.user.name&&obs.user.name!='')?(' ('+obs.user.name+')'):'');
@@ -41,7 +35,7 @@ function buildGrid(data) {
             let anchor = faddelem('a',gcell,{href:famp(obsurlbase+'/'+obs.id)});
             let photo = (photourl===null) ? faddelem('span',anchor,{innerText:'❎'})
                : faddelem('img',anchor,{style:{width:cellpx+'px',height:cellpx+'px'},
-                  title: taxon + ' on ' + taxon2 + ' observed by ' + user + ' on ' + obsdt,
+                  title: taxon + ' observed on ' + obsdt + ' observed by ' + user + ' on ' + obsdt,
                   src: ((cellpx<=75) ? photourl
                      : (cellpx<=150) ? photourl.replace('square','small')
                      : photourl.replace('square','medium'))
