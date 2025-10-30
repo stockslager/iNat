1b118512ca69a92f2f39bfc0b921cf4b72c38c8f
stockslager <149531737+stockslager@users.noreply.github.com>
2025-10-30 06:30:54 -0400

    Create observation.js

---
+++ js/plant_club_viewer/observation.js
@@ -0,0 +1,56 @@
+class Observation {
+  
+  constructor(rec) {
+     this.id                          = rec.id;
+    
+     if( rec.taxon ){
+         this.taxon_id                    = rec.taxon.id;
+         this.taxon_name                  = rec.taxon.name || '';
+         this.taxon_preferred_common_name = rec.taxon.preferred_common_name || '';
+         this.taxon_default_photo         = rec.taxon.default_photo || '';
+
+         if( rec.taxon.default_photo ){
+             if( rec.taxon.default_photo.url ){
+                 this.taxon.default_photo_url = rec.taxon.default_photo.url;
+             } 
+         }  
+     }
+       
+     this.photos                      = rec.photos || '';
+     if( rec.photos && rec.photos.length>0 ) {
+         this.photos_url = rec.photos[0].url || '';
+     } else {
+         this.photos_url = '';
+     }
+       
+     this.user_icon                   = rec.user.icon || '';
+     this.user_login                  = rec.user.login || '';
+       
+
+     this.ofvs = [];
+       
+     if( rec.ofvs && rec.ofvs.length>0 ) {
+         for( let i=0; i<rec.ofvs.length; i++ ) {
+              const obs_field = new ObsField(rec.ofvs[i]);
+              this.ofvs.push(obs_field);
+         }
+     }
+  }
+}
+
+class ObsField {
+  
+  constructor(obs_field) {
+     this.field_id         = obs_field.field_id;
+     this.value            = obs_field.value;
+     this.datatype         = obs_field.datatype;
+     this.taxon            = obs_field.taxon;
+
+     if( obs_field.taxon ) {
+         this.taxon_id                    = obs_field.taxon.id;
+         this.taxon_name                  = obs_field.taxon.name || '';
+         this.taxon_preferred_common_name = obs_field.taxon.preferred_common_name || '';
+         this.taxon_iconic_taxon_id       = obs_field.taxon.iconic_taxon_id || '';
+     }
+  }    
+}
