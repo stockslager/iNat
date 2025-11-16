e1962b456432e1d747a4064c30b042b1b7416090
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-16 11:15:54 -0500

    Update obs_results_common.js

--- test/observation_fields/js/obs_results_common.js
+++ test/observation_fields/js/obs_results_common.js
@@ -107,7 +107,7 @@ function buildFilterParams( chosen_taxon_id, field, field_value, taxon_name, fie
 }
 
 function getTaxonName( configuration, taxon_id ) {
-   let taxon_name = capitalizeWords(p_field_name) || 'Fauna';
+   let taxon_name = capitalizeWords(p_field_name) || 'Flora';
 
    for( let i=0; i<configuration.taxa.length; i++ ) {
         if( taxon_id === configuration.taxa[i].taxon_id ) {
