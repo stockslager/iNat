2f763e7933b7a098e0f5790264b6e9e44f8376be
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-16 07:22:14 -0500

    Update obs_results_common.js

--- test/observation_fields/js/obs_results_common.js
+++ test/observation_fields/js/obs_results_common.js
@@ -91,7 +91,6 @@ function showRow( rec ) {
 function buildFilterParams( chosen_taxon_id, field, field_value ) {
       let copyurlparams = new URLSearchParams(JSON.parse(JSON.stringify(Object.fromEntries(winurlparams))))
 
- console.log('one ' + copyurlparams);
       copyurlparams.delete('chosen_taxon_id');
       copyurlparams.delete('field_value');
       copyurlparams.delete('field');
