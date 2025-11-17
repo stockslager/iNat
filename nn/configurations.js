430fd73b299de27c83385d270e75f72bad52e22b
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-17 08:03:18 -0500

    Update configurations.js

--- nn/configurations.js
+++ nn/configurations.js
@@ -1,22 +1,34 @@
-async function asyncGetConfigurations() {
-   console.log('json ' + json_root+p_params);
-   await fetch((json_root+p_params+'.json'))
-     .then(response => response.json())
-     .then(data => {
-      // Access your data here 
-      for( let i=0; i<data.configurations.length; i++ ) {
+async function asyncGetConfigurations(params) {
+   console.log('fetching json: ' + json_root + params);
+
+   try {
+      const response = await fetch(json_root + params + '.json');
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      
+      // Access and manipulate global variables if necessary
+      /*for( let i=0; i<data.configurations.length; i++ ) {
            if( data.configurations[i].insect_project === p_project_id ) {
                interactions = data.configurations[i].interactions;
            }
            if( data.configurations[i].art_project === p_project_id ) {
                configuration = data.configurations[i];
            }
-      }
-      console.log(data);
-   })
-     .catch(error => {
+      }*/
+      
+      // Return the data object when successful
+      return data; 
+
+   } catch (error) {
       console.error('Error fetching JSON:', error);
-   });
+      
+      // Re-throw the error so the caller can catch it and handle it
+      throw error; 
+   }
 }
 
 // need to pull resources from a json file to pass into an api
