2fe70d35c21b18eda36701bc0565e43d8c5c1a8e
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-18 13:42:37 -0500

    Create roots.js

---
+++ nn/core/js/roots.js
@@ -0,0 +1,22 @@
+// root directory for inat observations
+let root_observations = 'https://www.inaturalist.org/observations/';
+let root_people       = 'https://www.inaturalist.org/people/';
+let root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
+let root_projects     = 'https://www.inaturalist.org/projects/';
+let root_taxa         = 'https://www.inaturalist.org/taxa/';
+let root_places       = 'https://www.inaturalist.org/places/';
+
+// repo root directory
+let github_root = 'https://stockslager.github.io/iNat/';
+
+// json root dir
+let json_root = 'https://stockslager.github.io/iNat/nn/core/json/';
+
+// JavaScript root dir 
+let js_root = 'https://stockslager.github.io/iNat/nn/core/js/';
+
+// outlinks used in cncnpc_seeds_fields.html
+// usda outlink 
+// missour botanical garden outlink 
+let root_usda = 'https://plants.usda.gov/plant-profile/';
+let root_missouri = 'https://www.missouribotanicalgarden.org/PlantFinder/PlantFinderDetails.aspx?kempercode=';
