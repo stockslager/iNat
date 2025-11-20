5fcc3b9ad2d79ee41662e34e18d3b78c8e24f2aa
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-20 08:14:26 -0500

    Create global_const.js

---
+++ nn/core/js/global_const.js
@@ -0,0 +1,13 @@
+// global constants...
+const CONST_TOTAL_SPECIES  = 'total species: ';  // common api results header
+const CONST_PAGE           = 'page: ';           // common api results header
+const CONST_PER_PAGE       = 'per page: ';       // common api results header
+const CONST_ON_ALL_PLANTS  = 'On All Plants';
+
+// show menu
+const CONST_ALL            = 'all';              // common show drop down label
+const CONST_SHOW           = 'Show';             // common show drop down title
+
+// show menu and home utf 8's
+const CONST_PLUS_UTF8      = '&#10133;'          // plus symbol for show drop down
+const CONST_LEAF_UTF8      = '&#127807;';        // home icon
