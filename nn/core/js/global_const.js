ff2637b58a5449049e1abf4fa6eee1786dcbf5c2
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-20 21:24:47 -0500

    Update CONST_BOOT_UTF8 to include HTML span tag

--- nn/core/js/global_const.js
+++ nn/core/js/global_const.js
@@ -13,6 +13,6 @@ const CONST_SHOW           = 'Show';             // common show drop down title
 const CONST_PLUS_UTF8      = '&#10133;'          // plus symbol for show drop down
 const CONST_SUNFLOWER_UTF8 = '&#127803;'         // back icon for garden list
 const CONST_LEAF_UTF8      = '&#127807;';        // back icon for yard list
-const CONST_BOOT_UTF8      = '&#x1F97E;';        // back icon for thru hiker
+const CONST_BOOT_UTF8      = '<span font-size:18px;>&#x1F97E;</span>';        // back icon for thru hiker
 const CONST_NPC_UTF8       = '&#128100;';        // back icon for observers
 const CONST_ART_UTF8       = '&#127912;';        // back icon for journaling / artwork
