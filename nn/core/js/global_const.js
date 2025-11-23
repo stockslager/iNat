bcd0a20c46c008a4d410092029792840fbbeaa15
stockslager <149531737+stockslager@users.noreply.github.com>
2025-11-23 15:22:15 -0500

    Update global_const.js

--- nn/core/js/global_const.js
+++ nn/core/js/global_const.js
@@ -16,3 +16,8 @@ const CONST_LEAF_UTF8      = '&#127807;';        // back icon for yard list
 const CONST_BOOT_UTF8      = '<span style="font-size: 18px;">&#x1F97E;</span>';        // back icon for thru hiker
 const CONST_NPC_UTF8       = '&#128100;';        // back icon for observers
 const CONST_ART_UTF8       = '&#127912;';        // back icon for journaling / artwork
+
+// iNat api per_page (number of rows per_page requested from api)...
+const CONST_OBSERVATIONS_PER_PAGE           = '10';
+const CONST_SPECIES_COUNTS_PER_PAGE         = '10';
+const CONST_OBSERVATIONS_OBSERVERS_PER_PAGE = '10';
