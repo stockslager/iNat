239de073b6e1d3500a932965e9a9f6cb86429de0
stockslager <149531737+stockslager@users.noreply.github.com>
2026-02-01 09:12:36 -0500

    Add seed icon constant and update comments

--- nn/core/js/global_const.js
+++ nn/core/js/global_const.js
@@ -11,11 +11,12 @@ const CONST_SHOW           = 'Show';             // common show drop down title
 
 // show menu and home utf 8's
 const CONST_PLUS_UTF8      = '&#10133;'          // plus symbol for show drop down
-const CONST_SUNFLOWER_UTF8 = '&#127803;'         // back icon for garden list
-const CONST_LEAF_UTF8      = '&#127807;';        // back icon for yard list
+const CONST_SUNFLOWER_UTF8 = '&#127803;'         // sunflower icon for garden list
+const CONST_LEAF_UTF8      = '&#127807;';        // leaf icon for yard list
+const CONST_SEED_UTF8      = '&#x1F330;';        // seed icon
 const CONST_BOOT_UTF8      = '<span style="font-size: 18px;">&#x1F97E;</span>';        // back icon for thru hiker
-const CONST_NPC_UTF8       = '&#128100;';        // back icon for observers
-const CONST_ART_UTF8       = '&#127912;';        // back icon for journaling / artwork
+const CONST_NPC_UTF8       = '&#128100;';        // npc icon for observers
+const CONST_ART_UTF8       = '&#127912;';        // pallete icon for journaling / artwork
 
 // iNat api per_page (number of rows per_page requested from api)...
 const CONST_OBSERVATIONS_PER_PAGE           = '10';
