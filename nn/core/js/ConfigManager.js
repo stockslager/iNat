a130ca143badc8d2b31822ad519c158694f41674
stockslager <149531737+stockslager@users.noreply.github.com>
2026-02-06 08:27:56 -0500

    Update ConfigManager.js

--- nn/core/js/ConfigManager.js
+++ nn/core/js/ConfigManager.js
@@ -58,6 +58,7 @@ class ConfigurationItem {
         this.plantListUserId = configData.plant_list_user_id ?? null;
         this.plantTagName    = configData.plant_tag_name ?? null;
         this.usePlantProjectImages = configData.use_plant_project_images ?? null;
+        this.lsMapExtent = configData.ls_mapextent ?? null;
         this.defaultPlace = configData.default_place ?? null;
         this.fieldId = configData.field_id ?? null;
         this.fieldName = configData.field_name ?? null;
