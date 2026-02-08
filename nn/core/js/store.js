452589ca5ca735c7e67b84286f55eecf9f611644
stockslager <149531737+stockslager@users.noreply.github.com>
2026-02-08 05:27:24 -0500

    Update store.js

--- nn/core/js/store.js
+++ nn/core/js/store.js
@@ -5,6 +5,7 @@ const COMPONENT_OBSERVERS = 'observers';
 const COMPONENT_HIKER     = 'hiker';
 const COMPONENT_YARD      = 'yard';
 const COMPONENT_ART       = 'art';
+const COMPONENT_FIELDS    = 'fields';
 
 // --- Constants for Attribute Keys ---
 const ATTRIBUTE_PROJECT         = 'project';
@@ -515,6 +516,33 @@ function validateColony(state) {
   if( message ) { return message; }
 
   if( getComponent(state) !== COMPONENT_COLONY ) {
+      let message = 'Component name required.  Component name is required for displaying data.' +   
+                    'Component name should exist in the configuration file identified by the &amp;params=xxxxx';
+      return message;
+  }
+
+  return;
+}
+
+/*
+ * component based validation
+ *
+ * Validates the state for fields-specific requirements.
+ * Ensures the 'params' attribute has a value.
+ * Ensures the 'component' attribute has a value. 
+ * Everything else should come from the configuration.
+ * Configuration is validated in configuration.js
+ * 
+ * @param {object} state The current application state object.
+ * @returns {string} An error message if invalid, otherwise an empty string.
+ */
+function validateFields(state) {
+  let message = '';
+  
+  message = validateConfig(state);
+  if( message ) { return message; }
+
+  if( getComponent(state) !== COMPONENT_FIELDS ) {
       let message = 'Component name required.  Component name is required for displaying data.' +   
                     'Component name should exist in the configuration file identified by the &amp;params=xxxxx';
       return message;
