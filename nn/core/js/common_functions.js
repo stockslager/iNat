281770bca8ae36d80c637374bd32309b449d6428
stockslager <149531737+stockslager@users.noreply.github.com>
2026-02-21 12:10:56 -0500

    Update common_functions.js

--- nn/core/js/common_functions.js
+++ nn/core/js/common_functions.js
@@ -1,25 +1,25 @@
 // root directory for inat observations
-let root_observations = 'https://www.inaturalist.org/observations/';
-let root_people       = 'https://www.inaturalist.org/people/';
-let root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
-let root_projects     = 'https://www.inaturalist.org/projects/';
-let root_taxa         = 'https://www.inaturalist.org/taxa/';
-let root_places       = 'https://www.inaturalist.org/places/';
+const root_observations = 'https://www.inaturalist.org/observations/';
+const root_people       = 'https://www.inaturalist.org/people/';
+const root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
+const root_projects     = 'https://www.inaturalist.org/projects/';
+const root_taxa         = 'https://www.inaturalist.org/taxa/';
+const root_places       = 'https://www.inaturalist.org/places/';
 
 // repo root directory
-let github_root = 'https://stockslager.github.io/iNat/';
+const github_root = 'https://stockslager.github.io/iNat/';
 
 // json root dir
-let json_root = 'https://stockslager.github.io/iNat/nn/core/json/';
+const json_root = 'https://stockslager.github.io/iNat/nn/core/json/';
 
 // JavaScript root dir 
-let js_root = 'https://stockslager.github.io/iNat/nn/core/js/';
+const js_root = 'https://stockslager.github.io/iNat/nn/core/js/';
 
 // outlinks used in cncnpc_seeds_fields.html
 // usda outlink 
 // missour botanical garden outlink 
-let root_usda = 'https://plants.usda.gov/plant-profile/';
-let root_missouri = 'https://www.missouribotanicalgarden.org/PlantFinder/PlantFinderDetails.aspx?kempercode=';
+const root_usda = 'https://plants.usda.gov/plant-profile/';
+const root_missouri = 'https://www.missouribotanicalgarden.org/PlantFinder/PlantFinderDetails.aspx?kempercode=';
 
 // global constants...
 const CONST_TOTAL_SPECIES  = 'total species: ';  // common api results header
