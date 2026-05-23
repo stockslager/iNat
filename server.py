6bcac83c39cc71aa5d9aafc7d7b8b654a7e0c7e5
stockslager <149531737+stockslager@users.noreply.github.com>
2026-05-23 16:21:28 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from http.server import AppendingHTTPRequestHandler, HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(b"<h1>Hello, World! From Python</h1>")
+        self.wfile.write(b"<h1>Hello, World! From Python on my Chromebook</h1>")
 
 server = HTTPServer(('0.0.0.0', 8000), MyHandler)
 print("Server running on port 8000...")
