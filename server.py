89d3609c285bfcd218413f020397c63b759a1a20
stockslager <149531737+stockslager@users.noreply.github.com>
2026-06-16 00:37:42 +0000

    final save

--- server.py
+++ server.py
@@ -1,12 +1,1 @@
-from http.server import AppendingHTTPRequestHandler, HTTPServer, BaseHTTPRequestHandler
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"<h1>Hello, World! From Python</h1>")
-
-server = HTTPServer(('0.0.0.0', 8000), MyHandler)
-print("Server running on port 8000...")
-server.serve_forever()
