99f4b6effd8fdc634504c8cf35736f34ab0e5a3c
stockslager <149531737+stockslager@users.noreply.github.com>
2026-07-05 14:40:37 -0400

    Fix formatting in common_functions.js

--- nn/core/js/common_functions.js
+++ nn/core/js/common_functions.js
@@ -3,7 +3,7 @@ const root_observations = 'https://www.inaturalist.org/observations/';
 const root_people       = 'https://www.inaturalist.org/people/';
 const root_obs_fields   = 'https://www.inaturalist.org/observation_fields/';
 const root_projects     = 'https://www.inaturalist.org/projects/';
-const root_taxa         = 'https://www.inaturalist.org/taxa/';
+const root_taxa         = 'https://www.inaturalist.org/taxa/'; 
 const root_places       = 'https://www.inaturalist.org/places/';
 const root_grid_cell    = 'https://www.inaturalist.org/observations';
 
