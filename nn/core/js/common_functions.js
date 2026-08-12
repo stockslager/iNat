0876ac0ea1e01f5fa8f52b0c1476e2893e668b8e
stockslager <149531737+stockslager@users.noreply.github.com>
2026-08-12 06:53:08 -0400

    Add root_identify URL for observation identification

--- nn/core/js/common_functions.js
+++ nn/core/js/common_functions.js
@@ -6,6 +6,7 @@ const root_projects     = 'https://www.inaturalist.org/projects/';
 const root_taxa         = 'https://www.inaturalist.org/taxa/'; 
 const root_places       = 'https://www.inaturalist.org/places/';
 const root_grid_cell    = 'https://www.inaturalist.org/observations';
+const root_identify     = 'https://www.inaturalist.org/observations/identify';
 
 // json root dir
 const json_root = '../core/json/';
