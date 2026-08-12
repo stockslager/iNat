82f57c642283b33a56cfabd5394785b8669300d2
stockslager <149531737+stockslager@users.noreply.github.com>
2026-08-11 23:50:34 -0400

    Modify furlCellBlank to include console log
    Added a console log statement to furlCellBlank function.

--- nn/core/js/grid.js
+++ nn/core/js/grid.js
@@ -1,5 +1,5 @@
 function furlCell(params,txt=url) { return '<a href="'+root_grid_cell+params+'">'+txt+'</a>'; };
-function furlCellBlank(params, txt) { return '<a href="' + root_grid_cell + params + '" target="_blank" rel="noopener noreferrer">' + txt + '</a>'; }
+function furlCellBlank(params, txt) { console.log('sdfasdf'); return '<a href="' + root_grid_cell + params + '" target="_blank" rel="noopener noreferrer">' + txt + '</a>'; }
   
 function buildGrid(data, url=null, config=null) {
 
