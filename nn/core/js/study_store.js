3e041690e6b02b08de6de003c2af45361974a8dd
stockslager <149531737+stockslager@users.noreply.github.com>
2026-08-18 13:51:07 -0400

    Export functions for Node.js module usage

--- nn/core/js/study_store.js
+++ nn/core/js/study_store.js
@@ -1031,3 +1031,8 @@ function clearForSpeciesCountsParams(state) {
 
   return urlState;
 }
+
+// tells Node.js what functions are allowed to be extracted for a nightly cron job
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { asyncGetConfiguration, apiFetch };
+}
